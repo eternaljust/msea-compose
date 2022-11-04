765f2f9f2fc7ef50642b71bb59d2a2fda8cf9510
eternaljust <tzqiang1118@163.com>
2022-11-04 13:31:57 +0800

    完善标签列表页面

--- app/src/main/java/com/eternaljust/msea/ui/page/node/tag/TagListPage.kt
+++ app/src/main/java/com/eternaljust/msea/ui/page/node/tag/TagListPage.kt
@@ -1,13 +1,8 @@
 package com.eternaljust.msea.ui.page.node.tag
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -18,16 +13,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemsIndexed
-import com.eternaljust.msea.ui.page.notice.system.SystemListItemContent
+import coil.compose.AsyncImage
+import com.eternaljust.msea.R
 import com.eternaljust.msea.ui.widget.RefreshList
 import com.eternaljust.msea.ui.widget.mseaTopAppBarColors
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun TagListPage(
     scaffoldState: SnackbarHostState,
@@ -67,6 +65,7 @@ fun TagListPage(
             Surface(
                 modifier = Modifier
                     .padding(paddingValues)
+                    .padding(horizontal = 16.dp)
             ) {
                 val viewStates = viewModel.viewStates
                 val lazyPagingItems = viewStates.pagingData.collectAsLazyPagingItems()
@@ -74,11 +73,123 @@ fun TagListPage(
                 RefreshList(
                     lazyPagingItems = lazyPagingItems
                 ) {
+                    stickyHeader {
+                        TagListHeader()
+                    }
+
                     itemsIndexed(lazyPagingItems) { _, item ->
-                        Text(text = item!!.title)
+                        item?.let {
+                            TagListItemContent(item = it)
+                        }
                     }
                 }
             }
         }
     )
+}
+
+@Composable
+fun TagListHeader() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "作者")
+
+            Text(text = "版块")
+
+            Text(text = "最后发表")
+
+            Text(text = "回复/查看")
+        }
+    }
+}
+
+@Composable
+fun TagListItemContent(item: TagListModel) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            AsyncImage(
+                modifier = Modifier
+                    .size(20.dp),
+                model = item.gif,
+                placeholder = painterResource(id = R.drawable.icon),
+                contentDescription = null
+            )
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column {
+                    Text(
+                        text = item.name,
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Text(
+                        text = item.time,
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+
+                Text(text = item.forum)
+
+                Column {
+                    Text(
+                        text = item.lastName,
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Text(
+                        text = item.lastTime,
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+
+                Text(
+                    modifier = Modifier
+                        .background(
+                            color = MaterialTheme.colorScheme.secondary,
+                            shape = RoundedCornerShape(50)
+                        )
+                        .padding(horizontal = 5.dp),
+                    text = "${item.reply}/${item.examine}",
+                    color = Color.White
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(text = item.title)
+    }
+
+    Divider(modifier = Modifier)
 }
--- app/src/main/java/com/eternaljust/msea/ui/page/node/tag/TagListViewModel.kt
+++ app/src/main/java/com/eternaljust/msea/ui/page/node/tag/TagListViewModel.kt
@@ -54,9 +54,54 @@ class TagListViewModel : ViewModel() {
                 println("td---${it.html()}")
                 var tag = TagListModel()
 
-                val gif = it.selectXpath("td[@class='icn']/a/img").text()
+                val gif = it.selectXpath("td[@class='icn']/a/img").attr("src")
                 if (gif.isNotEmpty()) {
                     tag.gif = HTMLURL.BASE + "/${gif}"
+                }
+                val title = it.selectXpath("th/a").text()
+                if (title.isNotEmpty()) {
+                    tag.title = title
+                }
+                val tid = it.selectXpath("th/a").attr("href")
+                if (tid.isNotEmpty()) {
+                    tag.tid = tid
+                }
+                val forum = it.selectXpath("td[@class='by']/a").text()
+                if (forum.isNotEmpty()) {
+                    tag.forum = forum
+                }
+                val fid = it.selectXpath("td[@class='by']/a").attr("href")
+                if (fid.isNotEmpty()) {
+                    tag.fid = fid
+                }
+                val name = it.selectXpath("td[@class='by'][2]/cite/a").text()
+                if (name.isNotEmpty()) {
+                    tag.name = name
+                }
+                val uid = it.selectXpath("td[@class='by'][2]/cite/a").attr("href")
+                if (uid.isNotEmpty()) {
+                    tag.uid = uid
+                }
+                val time = it.selectXpath("td[@class='by']/em/span").text()
+                if (time.isNotEmpty()) {
+                    tag.time = time
+                }
+                val reply = it.selectXpath("td[@class='num']/a").text()
+                if (reply.isNotEmpty()) {
+                    tag.reply = reply
+                }
+                val examine = it.selectXpath("td[@class='num']/em").text()
+                if (examine.isNotEmpty()) {
+                    tag.examine = examine
+                }
+                val lastName = it.selectXpath("td[@class='by'][last()]/cite/a").text()
+                if (lastName.isNotEmpty()) {
+                    tag.lastName = lastName
+                    tag.lastUserName = "space-username-${lastName}.html"
+                }
+                val lastTime = it.selectXpath("td[@class='by'][last()]/em/a").text()
+                if (lastTime.isNotEmpty()) {
+                    tag.lastTime = lastTime
                 }
 
                 list.add(tag)
@@ -88,16 +133,56 @@ sealed class TagListViewAction {
 }
 
 class TagListModel {
-    var uid = ""
-    var fid = ""
+    /**
+     * 帖子标题
+     */
     var title = ""
+    /**
+     * 帖子链接 id
+     */
     var tid = ""
+    /**
+     * 帖子动图
+     */
     var gif = ""
-    var plate = ""
+    /**
+     * 帖子板块
+     */
+    var forum = ""
+    /**
+     * 帖子板块 id
+     */
+    var fid = ""
+    /**
+     * 帖子作者
+     */
     var name = ""
+    /**
+     * 帖子作者 id
+     */
+    var uid = ""
+    /**
+     * 帖子发表时间
+     */
     var time = ""
+    /**
+     * 查看
+     */
     var examine = ""
+    /**
+     * 回复
+     */
     var reply = ""
+    /**
+     * 最后发表的昵称
+     */
     var lastName = ""
+    /**
+     * 最后发表的用户
+     */
+    var lastUserName = ""
+    /**
+     * 最后发表的时间
+     */
     var lastTime = ""
 }
